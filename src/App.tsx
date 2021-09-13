a20f9bd94f724f2fafe7103d3f7e6f4505cb81fe
Evan Rosson <git@erosson.org>
2021-09-13 05:55:50 -0400

    automerge/memory db routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,47 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { LocalizationProvider } from '@fluent/react';
 import l10n from './Locale'
 
-// import DBAutomerge from './DB/Automerge'
-// import DBMemory from './DB/Memory'
+import DBAutomerge from './DB/Automerge'
+import DBMemory from './DB/Memory'
 import DBDexie from './DB/Dexie'
 import DBUserbase from './DB/Userbase'
 import * as Routes from './Routes'
 
 export function App() {
+  if (document.location.pathname.startsWith('/db/automerge')) {
+    return (
+      <Router basename="/db/automerge">
+        <AppRoutes>
+          <DBAutomerge routes={Routes.routes} />
+          db-automerge
+        </AppRoutes>
+      </Router>
+    )
+  }
+  else if (document.location.pathname.startsWith('/db/memory')) {
+    return (
+      <Router basename="/db/memory">
+        <AppRoutes>
+          <DBMemory routes={Routes.routes} />
+          db-memory
+        </AppRoutes>
+      </Router>
+    )
+  }
   return (
     <Router>
       <AppRoutes />
     </Router>
   )
 }
-export function AppRoutes() {
+export function AppRoutes(p: {children?: React.ReactNode}) {
   return (
-    // <LocalizationProvider l10n={l10n}>
-      // <DBAutomerge routes={Routes.routes} />
-    // </LocalizationProvider>
-    // <LocalizationProvider l10n={l10n}>
-      // <DBMemory routes={Routes.routes} />
-    // </LocalizationProvider>
     <LocalizationProvider l10n={l10n}>
-      <DBUserbase>
-        <DBDexie routes={Routes.routes} />
-      </DBUserbase>
+      {p.children ? <>{p.children}</> :
+        <DBUserbase>
+          <DBDexie routes={Routes.routes} />
+        </DBUserbase>
+      }
     </LocalizationProvider>
   )
 }
